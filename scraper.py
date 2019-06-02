b5167dc2bf3b91e3570d3c58c48d68231ae4e3c1
squigglezworth <squigglezworth@gmail.com>
2019-06-01 21:00:16 -0400

    Fix line endings (broken diff :()

--- scraper.py
+++ scraper.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 from requests_html import HTMLSession
 from requests import get
 import urllib.request
@@ -28,17 +26,23 @@ print('\nFound {} books. Building URLs...'.format(len(results)))
 
 for x in results:
 	info = x.split('/')
-	url = "{}/dist/{}_{}.epub".format(x, info[2], info[3])
-	links.append(url)
+	del info[:2]
+
+	url = "{}/dist/{}_{}.epub".format(x, info[0], info[1])
+	info.append(url)
+	links.append(info)
 
 print('Done building URLs')
 
-for x in links:
-	print('\rGrabbing book {} of {}'.format(links.index(x) + 1, len(links)), end='')
+for book in links:
+	title = book[1].replace('-', ' ').title()
+	author = book[0].replace('-', ' ').title()
 
-	info = x.split('/')
+	print('\rGrabbing book {} by {}'.format(title, author, end=''))
+
+	info = book[-1].split('/')
 	with open('{}/{}'.format(os.getcwd(), info[-1]), 'wb+') as file:
-		r = get(base_url + x)
+		r = get(base_url + book[-1])
 		file.write(r.content)
 
 print('Done! Enjoy!')
